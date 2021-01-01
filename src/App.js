c12dc5a776069c99a3d8ef9f6a0889ae44fadf81
maksstepak <maks.stepak1@gmail.com>
2021-01-02 00:47:06 +0100

    Add auth slice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { auth } from './services/firebase';
+import { login, logout } from './slices/auth';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(login(user));
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
     <div className="App">
       <h2>It works</h2>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../slices/auth';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
